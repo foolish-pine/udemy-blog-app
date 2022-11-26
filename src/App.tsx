2dcc36b153dcde88e4cd7e6b5aebf9a7c7167bf8
foolish-pine <matsuda.takuya41@gmail.com>
2022-11-26 22:30:43 +0900

    cp32

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CreatePost } from "components/CreatePost";
 import { Home } from "components/Home";
@@ -9,13 +9,16 @@ import { Navbar } from "components/Navbar";
 import "App.css";
 
 export const App: FC = () => {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [isAuth, setIsAuth] = useState(false);
+
 	return (
 		<Router>
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/createpost" element={<CreatePost />}></Route>
-				<Route path="/login" element={<Login />}></Route>
+				<Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
 				<Route path="/logout" element={<Logout />}></Route>
 			</Routes>
 		</Router>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,28 @@
 import { FC } from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "firestore";
+import { useNavigate } from "react-router-dom";
 
-export const Login: FC = () => {
-	return <div>Login</div>;
+type Props = {
+	setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const Login: FC<Props> = ({ setIsAuth }) => {
+	const navigate = useNavigate();
+	const loginInWithGoogle = () => {
+		// Googleでログイン
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+		void signInWithPopup(auth, provider).then((result) => {
+			localStorage.setItem("isAuth", "true");
+			setIsAuth(true);
+			navigate("/");
+		});
+	};
+
+	return (
+		<div>
+			<p>ログインして始める</p>
+			<button onClick={loginInWithGoogle}>Googleでログイン</button>
+		</div>
+	);
 };
